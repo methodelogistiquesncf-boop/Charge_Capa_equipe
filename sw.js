e513154632b78a09a0da615d39d74db865630e82
methodelogistiquesncf-boop <methodelogistiquesncf@gmail.com>
2026-09-08 14:58:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,24 @@
-const CACHE = 'pa-cache-v8';
-const SHELL = [
-  './',
-  './index.html',
-  './aide.html',
-  './firebase.js',
-  './manifest.json',
-  './icons/logo.svg',
-  './icons/icon-512.png'
-];
+const CACHE = 'pa-cache-v10';
+const SHELL = ['./', './index.html', './aide.html', './firebase.js', './manifest.json', './icons/logo.svg', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => Promise.allSettled(SHELL.map(u => c.add(u))))
-      .then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(SHELL.map(u => c.add(u)))).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
   if (e.request.mode === 'navigate') {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res; })
-        .catch(() => caches.match('./index.html'))
-    );
+    e.respondWith(fetch(e.request).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res; }).catch(() => caches.match('./index.html')));
     return;
   }
   if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-        const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res;
-      }))
-    );
+    e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res; })));
     return;
   }
-  e.respondWith(
-    caches.match(e.request).then(hit => {
-      const fresher = fetch(e.request).then(res => {
-        const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res;
-      }).catch(() => hit);
-      return hit || fresher;
-    })
-  );
+  e.respondWith(caches.match(e.request).then(hit => {
+    const fresher = fetch(e.request).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return res; }).catch(() => hit);
+    return hit || fresher;
+  }));
 });
