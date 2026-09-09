77a4236d09c3f9af2fd69849e458e8cc731d2b16
methodelogistiquesncf-boop <methodelogistiquesncf@gmail.com>
2026-09-09 07:49:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pa-cache-v13';
+const CACHE = 'pa-cache-v14';
 const SHELL = ['./', './index.html', './aide.html', './firebase.js', './manifest.json', './icons/logo.svg', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
